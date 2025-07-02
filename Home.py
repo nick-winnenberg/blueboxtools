96949a8a8e66cb21adeb8675691e9b0a15fa9837
Nick Winnenberg <nick@winnenberg.org>
2025-07-02 10:45:43 -0400

    V.15: Added Features

--- Home.py
+++ Home.py
@@ -8,25 +8,28 @@ import altair as alt
 
 st.sidebar.success("☝️ Check out these bluebox tools.☝️")
 
-st.header("Updates V 0.1")
-st.text("- Added multi-sheet functionality to combine offices/sales reps into a single dataframe. AKA, you can upload multiple sheets at the same time.")
-st.text("- Added Turnover and Recruiting Analysis tools that utilize an exported activity report.")
-
 st.header("Welcome to Bluebox Developer Tools!")
 st.text("This is an updated directory, using cloud tools, to automate some developer reponsibilities. This code is maintained by Nick Winnenberg. Please email me or send me a teams message with any questions.")
 
+st.markdown("**Updates V 0.15**")
+st.markdown("- Added multi-sheet functionality to combine offices/sales reps into a single dataframe. AKA, you can upload multiple sheets at the same time.")
+st.markdown("- Added Turnover and Recruiting Analysis tools that utilize an exported activity report.")
 
-st.header("Activity Report Tools")
+st.markdown("##")
+
+st.subheader("Activity Report Tools",divider=True)
 st.text("All you need to do is export the activity report, then upload it into the tool. Magic!")
 st.subheader("Recruiting Analysis ❗")
-st.text("Use an exported activity report to understand the recruiting themses in an office.")
-st.subheader("Turnover Analysis 🎭")
 st.text("Use an exported activity report to understand the recruiting themes in an office.")
+st.subheader("Turnover Analysis 🎭")
+st.text("Use an exported activity report to understand the turnover themes in an office.")
 
-st.header("Client Prospect Activity Detail Tools ")
+st.markdown("##")
+
+st.subheader("Client Prospect Activity Detail Tools",divider=True)
 st.text("Exporting is tricky! Make sure you go to My Menu -> Preferences -> Sales Planner -> Client Prospect Activity Details filtered by Completed Date -> Move all the Columns Available to the Columns Chosen. Export, then drag and drop! Call me on Teams if you need help with this.")
 st.subheader("Client Pipeline 💸")
-st.text("Use an exported call summary to see which companies are being called on, and where that are in the pipeline (DM Connections, Appointments, etc.) It's a quick and easy way to have a conversation about a sales representatives current funnel")
+st.text("Use an exported call summary to see which companies are being called on, and the health of their pipeline (DM Connections, Appointments, etc.) It's a quick and easy way to have a conversation about a sales representatives current funnel")
 st.subheader("Sales Coach 🤓")
 st.text("Use an exported call summary to analyze the overall success of a sales representative, or an office, with a few additional sales metrics thrown into the game. Great high level overview.")
 st.subheader("Sales Rep Weekly Calls Report Card 🔎")
